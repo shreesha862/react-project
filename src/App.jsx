c80e4a5314fdaf0d0190720292c30cfba03277bb
shreesha862 <shreeshar71@gmail.com>
2026-02-20 10:16:15 +0530

    Remove node_modules from tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,29 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
-//import Login from "./assets/pages/Login.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import MyCourses from "./pages/MyCourses.jsx";
 import Progress from "./pages/Progress.jsx";
 import Notifications from "./pages/Notification.jsx";
 import Login from "./pages/Login.jsx";
 
-
-
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        {/* Login */}
         <Route path="/" element={<Login />} />
-
-        {/* Dashboard */}
         <Route path="/dashboard" element={<Dashboard />} />
-
-        {/* Courses */}
         <Route path="/my-courses" element={<MyCourses />} />
-
-        {/* Progress */}
         <Route path="/progress" element={<Progress />} />
-
-        {/* Notifications page */}
         <Route path="/notifications" element={<Notifications />} />
-        
-
-        {/* Redirect unknown routes */}
         <Route path="*" element={<Navigate to="/" />} />
-      
-
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
 export default App;
+
+
 
 
 
